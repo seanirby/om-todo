8b3b147a52fc4340243580d284d2b6af90930a28
Sean <sean.t.irby@gmail.com>
2016-02-12 11:07:45 -0800

    Basically done

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -1,140 +1,144 @@
 (ns om-tutorial.core
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [cljs.pprint :as pprint]))
 
 (enable-console-print!)
 
 (def init-data
-  {:list/one [{:name "John" :points 0}
-              {:name "Mary" :points 0}
-              {:name "Bob"  :points 0}]
-   :list/two [{:name "Mary" :points 0 :age 27}
-              {:name "Gwen" :points 0}
-              {:name "Jeff" :points 0}]})
+  {:dashboard/items
+   [{:id 0 :type :dashboard/post
+     :author "Laura Smith"
+     :title "A Post!"
+     :content "Lorem ipsum dolor sit amet, quem atomorum te quo"
+     :favorites 0}
+    {:id 1 :type :dashboard/photo
+     :title "A Photo!"
+     :image "photo.jpg"
+     :caption "Lorem ipsum"
+     :favorites 0}
+    {:id 2 :type :dashboard/post
+     :author "Jim Jacobs"
+     :title "Another Post!"
+     :content "Lorem ipsum dolor sit amet, quem atomorum te quo"
+     :favorites 0}
+    {:id 3 :type :dashboard/graphic
+     :title "Charts and Stufff!"
+     :image "chart.jpg"
+     :favorites 0}
+    {:id 4 :type :dashboard/post
+     :author "May Fields"
+     :title "Yet Another Post!"
+     :content "Lorem ipsum dolor sit amet, quem atomorum te quo"
+     :favorites 0}]})
 
-;; -----------------------------------------------------------------------------
-;; Parsing
+(defui Post
+  static om/IQuery
+  (query [this]
+    [:id :type :title :author :content])
+  Object
+  (render [this]
+    (let [{:keys [title author content] :as props} (om/props this)]
+      (dom/div nil
+        (dom/h3 nil title)
+        (dom/h4 nil author)
+        (dom/p nil content)))))
 
+(def post (om/factory Post))
 
-;;;Defines a multimethod using om/dispatch as the dispatcher
-;;;multimethod will dispatch on key
+(defui Photo
+  static om/IQuery
+  (query [this]
+    [:id :type :title :image :caption])
+  Object
+  (render [this]
+    (let [{:keys [title image caption]} (om/props this)]
+      (dom/div nil
+        (dom/h3 nil (str "Photo: " title))
+        (dom/div nil image)
+        (dom/p nil (str "Caption: " caption))))))
+
+(def photo (om/factory Photo))
+
+(defui Graphic
+  static om/IQuery
+  (query [this]
+    [:id :type :title :image])
+  Object
+  (render [this]
+    (let [{:keys [title image]} (om/props this)]
+      (dom/div nil
+        (dom/h3 nil (str "Graphic: " title))
+        (dom/div nil image)))))
+
+(def graphic (om/factory Graphic))
+
+(defui DashboardItem
+  static om/Ident
+  (ident [this {:keys [id type]}]
+    [type id])
+  static om/IQuery
+  (query [this]
+    (zipmap
+      [:dashboard/post :dashboard/photo :dashboard/graphic]
+      (map #(conj % :favorites)
+        [(om/get-query Post)
+         (om/get-query Photo)
+         (om/get-query Graphic)])))
+  Object
+  (render [this]
+    (let [{:keys [id type favorites] :as props} (om/props this)]
+      (dom/li
+        #js {:style #js {:padding 10 :borderBottom "1px solid black"}}
+        (dom/div nil
+          (dom/div #js {:style #js {:color "blue"}} "beans")
+          ;; post, photo, and graphic are all component factory functions
+          (({:dashboard/post    post
+             :dashboard/photo   photo
+             :dashboard/graphic graphic} type)
+           (om/props this)))
+        (dom/div nil
+          (dom/p nil (str "Favorites: " favorites))
+          (dom/button
+            #js {:onClick
+                 (fn [e]
+                   (om/transact! this
+                     `[(dashboard/favorite {:ref [~type ~id]})]))}
+            "Favorite!"))))))
+
+(def dashboard-item (om/factory DashboardItem))
+
+(defui Dashboard
+  static om/IQuery
+  (query [this]
+    [{:dashboard/items (om/get-query DashboardItem)}])
+  Object
+  (render [this]
+    (let [{:keys [dashboard/items]} (om/props this)]
+      (apply dom/ul
+        #js {:style #js {:padding 0}}
+        (map dashboard-item items)))))
+
 (defmulti read om/dispatch)
 
-;;; Gets the people, key is line/one or line/two
-(defn get-people [state key]
+(defmethod read :dashboard/items
+  [{:keys [state]} k _]
   (let [st @state]
-    (into [] (map #(get-in st %)) (get st key))))
-
-;;; two different lists
-(defmethod read :list/one
-  [{:keys [state] :as env} key params]
-  (println key)
-  {:value (get-people state key)})
-
-(defmethod read :list/two
-  [{:keys [state] :as env} key params]
-  {:value (get-people state key)})
+    {:value (into [] (map #(get-in st %)) (get st k))}))
 
 (defmulti mutate om/dispatch)
 
-;; the muatte signature is [env key params]
-(defmethod mutate 'points/increment
-  [{:keys [state]} _ {:keys [name]}]
+(defmethod mutate 'dashboard/favorite
+  [{:keys [state]} k {:keys [ref]}]
   {:action
    (fn []
-     (swap! state update-in
-       ;; whats being swapped here?
-       [:person/by-name name :points]
-       inc))})
-
-(defmethod mutate 'points/decrement
-  [{:keys [state]} _ {:keys [name]}]
-   {:action
-   (fn []
-     (swap! state update-in
-       [:person/by-name name :points]
-       #(let [n (dec %)] (if (neg? n) 0 n))))})
-
-;; -----------------------------------------------------------------------------
-;; Components
-
-(defui Person
-  static om/Ident
-  ;;; declares that :person/by-name are associated with i lo
-  (ident [this {:keys [name]}]
-    [:person/by-name name])
-  ;;; declares how this component gets its data
-  static om/IQuery
-  (query [this]
-    '[:name :points :age])
-  Object
-  (render [this]
-    (println "Render Person" (-> this om/props :name))
-    (let [{:keys [points name] :as props} (om/props this)]
-      (dom/li nil
-        (dom/label nil (str name ", points: " points))
-        (dom/button
-          #js {:onClick
-               (fn [e]
-                 (om/transact! this
-                   `[(points/increment ~props)]))}
-          "+")
-        (dom/button
-          #js {:onClick
-               (fn [e]
-                 (om/transact! this
-                   `[(points/decrement ~props)]))}
-          "-")))))
-
-(def person (om/factory Person {:keyfn :name}))
-
-(defui ListView
-  Object
-  (render [this]
-    ;; wtf does om/path do?
-    (println this)
-    ;; it looks like a a way to get a nested structed of things
-    (println (om/path this))
-    (println "props")
-    (println (om/props this))
-    (println "Render ListView" (-> this om/path first))'
-    (println "reconciler")
-    (println @reconciler)
-    (println 'b)
-    (let [list (om/props this)]
-      (apply dom/ul nil
-        (map person list)))))
-
-;; a thing that can create instances of ListView
-(def list-view (om/factory ListView))
-
-(defui RootView
-  static om/IQuery
-  (query [this]
-    (let [subquery (om/get-query Person)]
-      ;; I can refer to these keys later on with one and two
-      `[{:list/one ~subquery} {:list/two ~subquery}]))
-  Object
-  (render [this]
-    (println "Render RootView")
-    (let [{:keys [list/one list/two]} (om/props this)]
-      (println "one")
-      (println "beans")
-      (println one)
-      (apply dom/div nil
-        [(dom/h2 nil "List A")
-         (list-view one)
-         (dom/h2 nil "List B")
-         (list-view two)]))))
+     (swap! state update-in (conj ref :favorites) inc))})
 
 (def reconciler
-;the application state. If not an atom the reconciler will normalize the data with the query supplied by the root component.
   (om/reconciler
-    {:state  init-data
-     :parser (om/parser {:read read :mutate mutate})}))
+    {:state init-data
+     :parser (om/parser
+               {:read read :mutate mutate})}))
 
-(println "First reconciler:")
-(println @reconciler)
-(om/add-root! reconciler
-  RootView (gdom/getElement "app"))
+(om/add-root! reconciler Dashboard (gdom/getElement "app"))
